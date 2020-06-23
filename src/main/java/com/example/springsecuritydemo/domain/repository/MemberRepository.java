c7b38611edf7bc0d85f7683a3573b2d0ae690d37
enhakkore <ahn.ephraim@gmail.com>
2020-06-23 20:38:16 +0900

    MemberRepository 추가

---
+++ src/main/java/com/example/springsecuritydemo/domain/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.springsecuritydemo.domain.repository;
+
+import com.example.springsecuritydemo.domain.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String userEmail);
+}

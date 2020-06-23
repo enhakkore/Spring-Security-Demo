8a6ffc01774c706cd2d4f4a013cfb40c923a1e24
enhakkore <ahn.ephraim@gmail.com>
2020-06-23 20:43:52 +0900

    CustomConfig 추가

---
+++ src/main/java/com/example/springsecuritydemo/config/CustomConfig.java
@@ -0,0 +1,16 @@
+package com.example.springsecuritydemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableJpaAuditing
+@Configuration
+public class CustomConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
